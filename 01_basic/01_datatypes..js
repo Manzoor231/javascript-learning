172fc46adb4195bce5f07a4cdfa0408be87eed71
Manzoor Strange <strangemia7001@gmail.com>
2025-04-09 22:59:49 +0500

    basics of dataypes

---
+++ 01_basic/01_datatypes..js
@@ -0,0 +1,25 @@
+
+// Datatypes
+
+// string
+let name = "Omer";
+
+// numbers
+let age = 100;
+
+// boolean
+let isTrue = false;
+
+// array
+let myarr = ['name',23,'age']
+
+// null which is standard value but its datatypes is object
+let a = null;
+
+// undiefined 
+let b;
+
+//object
+let myObj = {
+    color: 'blue'
+}
